0a27aa5244d43548e5e2bca40e77a5f597d34683
Dmitry Grigoriev <dimgel@mail.ru>
2024-05-22 17:49:48 +0300

    purrsuit: fixed example02.

--- examples/02-validate/src/main/java/ru/dimgel/wf0/example02/DispatcherImpl.java
+++ examples/02-validate/src/main/java/ru/dimgel/wf0/example02/DispatcherImpl.java
@@ -4,6 +4,7 @@ import java.util.regex.Pattern;
 import jakarta.servlet.http.HttpServletRequest;
 import ru.dimgel.wf0.dispatch.Dispatcher;
 import ru.dimgel.wf0.dispatch.Page;
+import ru.dimgel.wf0.dispatch.RequestMethod;
 import ru.dimgel.wf0.example02.page.HomePage;
 
 
@@ -11,7 +12,7 @@ public class DispatcherImpl extends Dispatcher {
 	private final Pattern uriRegex = Pattern.compile("/(\\?.*)?");
 
 	@Override
-	public Page createPage(HttpServletRequest rq) {
+	public Page createPage(HttpServletRequest rq, RequestMethod m) {
 		return uriRegex.matcher(rq.getRequestURI()).matches()
 				? new HomePage()
 				: null;
